8e81c3dc911ecc8310b8a0deb49f9479de73e578
whyttte <fogboruche@yahoo.com>
2020-08-22 07:16:29 +0000

    rest of the remaining queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -133,6 +133,35 @@ const getAllProperties = function(options, limit = 10) {
     queryParams.push(`%${options.city}%`);
     queryString += `WHERE city LIKE $${queryParams.length} `;
   }
+  // for owner
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    if (queryParams.length === 1) {
+      queryString += `WHERE owner_id LIKE $${queryParams.length} `;
+    } else {
+      queryString += `AND owner_id = $${queryParams.length} `;
+    }
+  }
+
+  // for minimum/maximum pricing returns
+  if (options.minimum_price_per_night && options.maximum_price_per_night ) {
+    queryParams.push(`%${options.maximum_price_per_night, options.minimum_price_per_night}%`);
+    if (queryParams.length === 2) {
+      queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
+    } else {
+      queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
+    }
+  }
+
+
+  if (options.minimum_rating) {
+    queryParams.push(`%${options.minimum_rating}%`);
+    if (queryParams.length === 1) {
+      queryString += `WHERE minimum_rating >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
+    } else {
+      queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
+    }
+  }
 
   // 4
   queryParams.push(limit);
